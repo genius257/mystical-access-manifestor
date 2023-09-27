b5a48d89657823068c7772433d394a3818143316
Anders Pedersen <hot415@hotmail.com>
2023-09-27 05:19:54 +0000

    Added needed inline code documentation

--- src/PasswordDecorator.php
+++ src/PasswordDecorator.php
@@ -13,12 +13,18 @@ abstract class PasswordDecorator implements Generator
     public function generate(): string
     {
         $generated = $this->generator->generate();
+
+        // Find all occurences of the placeholder char
         preg_match_all('/\0/', $generated, $matches, PREG_OFFSET_CAPTURE);
         
+        // If no placeholder is found, just return the previous generated password
         if (empty($matches[0])) {
             return $generated;
         }
 
+        /**
+         * Random position(s) from the available placeholders
+         */
         $targets = array_rand($matches[0], $this->length);
         $chars = str_shuffle($this->chars);
         foreach ($targets as $index => $target) {
